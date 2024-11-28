2ae947f9ff34a3c328768ba7cb05af49099f324f
Thomashub26 <thomasmondragon44@gmail.com>
2024-11-28 10:52:21 -0500

    SpringBootFrmAlumno2

---
+++ src/main/java/com/example/demo/TareaSesion04Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaSesion04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaSesion04Application.class, args);
+	}
+
+}
